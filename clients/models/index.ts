a1572aa2ba579612917d1b2fd9ff45cacac83c32
amourRamanantsiresy <hei.amour.2@gmail.com>
2026-01-26 22:02:09 +0300

    chore: generate new client & docs

--- clients/models/index.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from "./CreationLabel";
-export * from "./Credentials";
-export * from "./Label";
-export * from "./PaginationResult";
-export * from "./SignInResult";
-export * from "./SignUpResult";
